e10c75d142050ae959126fd0431846a18a811f0e
merrymike-noname <kovalenko.mykhailo1@lll.kpi.ua>
2024-10-31 11:33:58 +0200

    Initial commit

---
+++ src/main/java/com/kovalenko/backendlab2/BackEndLab2Application.java
@@ -0,0 +1,13 @@
+package com.kovalenko.backendlab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndLab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kovalenko/backendlab2/BackEndLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovalenko.backendlab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndLab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
